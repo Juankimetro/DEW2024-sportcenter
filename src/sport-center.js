3fb6f5662b23f55835060b2148b1cfb67610b13b
Juankimetro <juanka3027@gmail.com>
2024-10-15 18:40:13 +0100

    Test 1

--- src/sport-center.js
+++ src/sport-center.js
@@ -1,5 +1,21 @@
 class SportCenter {
+  name
+  #services
+  membership = 0
+  fee
 
+  constructor (name, fee = 0, membership = 0) {
+    this.name = name
+    this.fee = fee
+    this.membership = membership
+    /* if (membership !== undefined) {
+      this.membership = membership
+    } */
+  }
+
+  income () {
+    return this.fee * this.membership
+  }
 }
 
 module.exports = SportCenter
