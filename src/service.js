60a79ca0681ca7c94832a420559964119bfc102b
Juankimetro <juanka3027@gmail.com>
2024-10-15 20:08:24 +0100

    Test 2

---
+++ src/service.js
@@ -0,0 +1,28 @@
+class service {
+  name
+  ratings = []
+
+  constructor (name, ratings = []) {
+    this.name = name
+    this.ratings = ratings
+  }
+
+  get rating () {
+    if (this.ratings.length === 0) {
+      return 0
+    }
+
+    let result = 0
+    this.ratings.forEach(rate => {
+      result += rate
+    })
+
+    return (((result / this.ratings.length) * 100)).toFixed(2)
+  }
+
+  giveRating (numero) {
+    this.ratings.push(numero)
+  }
+}
+
+module.exports = service
