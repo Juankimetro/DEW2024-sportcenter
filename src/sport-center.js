69181bc777cbdf121b6c80509bc478ffca415c87
Juankimetro <juanka3027@gmail.com>
2024-10-16 02:06:20 +0100

    Test 10

--- src/sport-center.js
+++ src/sport-center.js
@@ -1,6 +1,10 @@
+const Activity = require('./activity')
+const Facility = require('./facility')
+const Service = require('./service')
+
 class SportCenter {
   name
-  #services
+  #services = []
   membership = 0
   fee
 
@@ -15,6 +19,45 @@ class SportCenter {
 
   income () {
     return this.fee * this.membership
+  }
+
+  getServices () {
+    return this.#services
+  }
+
+  addService (...services) {
+    services.forEach(service => {
+      if (service instanceof Service && !this.#services.includes(service)) {
+        this.#services.push(service)
+      }
+    })
+  }
+
+  removeService (services) {
+    if (services instanceof Service) {
+      const index = this.#services.indexOf(services)
+      if (index >= 0) {
+        this.#services.splice(index, 1)
+      }
+    }
+  }
+
+  getFacilities () {
+    return this.#services.filter(s => s instanceof Facility)
+  }
+
+  getActivities () {
+    return this.#services.filter(s => s instanceof Activity)
+  }
+
+  orderServicesBy (criteria) {
+    switch (criteria) {
+      case 'name': this.#services.sort((a, b) => (a.name.toLowerCase() < b.name.toLowerCase() ? -1 : 1))
+        break
+      case 'rating': this.#services.sort((a, b) => (a.rating > b.rating ? -1 : 1))
+        break
+      default: break
+    }
   }
 }
 
